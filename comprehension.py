73b60293b706844f66c544630f03f3897634214e
ShauryaSingh1709 <shauryaa170904@gmail.com>
2026-06-08 10:00:57 +0530

    Shaurya

---
+++ comprehension.py
@@ -0,0 +1,12 @@
+#Comprehension are of three types :-
+#1. List
+#2. Sets
+#3. Dict
+
+#List Comprehension :- 
+abc = [i for i in range(1,200) if i % 2 == 0]
+print(abc)
+
+#Set Comprehension :-
+shau = {i for i in range(1,200) if i % 2 == 0}
+print(shau)
