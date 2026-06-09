3d7375f1c5452e71de4183cb098f784dd7f252d2
ShauryaSingh1709 <shauryaa170904@gmail.com>
2026-06-09 10:16:28 +0530

    Shaurya

---
+++ iterator.py
@@ -0,0 +1,4 @@
+#Iterator :- It is the process of traversing through sequence and getting the value one by one.
+# The function is used to perform iteration.
+# For loop is completely depend on iterator so that it has an inbuilt iterator.
+# To perform iterator it consist of two important method :- iter() and next()
