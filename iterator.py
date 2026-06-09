6cd6833fda8be87f8f060109b789ac62af344902
ShauryaSingh1709 <shauryaa170904@gmail.com>
2026-06-09 10:59:50 +0530

    Shaurya

--- iterator.py
+++ iterator.py
@@ -8,8 +8,29 @@ print(next(it))
 print(next(it))
 print(next(it))
 print(next(it))
-print(next(it)) #This will give error bcoz list is exhausted stopiteration error.
+# print(next(it)) #This will give error bcoz list is exhausted stopiteration error.
 print(it) #give address.
 
 #Iter() method :- It is a function which is used to make the control to get pointed to the initial node address.
 #Syntax for iter() is :- var_name = iter(iterable)
+
+#Next() method :- It is a function which is used to get the values one by one.
+#Once you access all the objects inside the iterable it will give stopiteration error.
+#Syntax for next() is :- next(iterator)
+
+class Count:
+    def __init__ (self,start,stop):
+        self.start = start
+        self.stop = stop
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.start >= self.stop:
+            raise StopIteration
+        value = self.start
+        self.start += 1
+        return value
+counter = Count(1,10)
+# print(next(counter)) this will print one by one.
+for i in counter:
+    print(i)
