aecf4832aec8f6c38f6cbee538c2cffc20189ffb
ShauryaSingh1709 <shauryaa170904@gmail.com>
2026-06-11 10:26:31 +0530

    Shaurya

---
+++ libraryandmodules.py
@@ -0,0 +1,13 @@
+#random :- 
+#1. random()
+#2. randint()
+#3. choice()
+#4. shuffle()
+#5. sample()
+
+import random
+print(random.random()) #It will give random value between the range of 0.0 to 1.0
+
+# print(random.randint(start value , end value))
+print(random.randint(1,6)) #It will give random value between the range of start value to end value. start value must be smaller than end value.
+
