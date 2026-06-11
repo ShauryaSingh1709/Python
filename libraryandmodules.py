dec1caf7b147c7af387a2cf9e932d505108aeb62
ShauryaSingh1709 <shauryaa170904@gmail.com>
2026-06-11 10:47:43 +0530

    Shaurya

--- libraryandmodules.py
+++ libraryandmodules.py
@@ -28,3 +28,21 @@ print(abc)
 #sample(iterable, k)
 shau = ['apple', 'mango', 'banana', 'grapes']
 print(random.sample(shau, 2))
+
+
+
+import math
+
+print(math.ceil(4.7)) #output :- 5
+print(math.floor(4.7)) #output :- 4
+print(math.pow(2,3)) #output :- 8 here 2 will base and 3 will power
+print(math.factorial(5)) #output :- 120
+print(math.isqrt(726)) #output :- 26
+print(math.sqrt(726))  #output :- 26.94438717061496
+print(math.sqrt(144)) #output :- 12.0
+print(math.isqrt(144)) #output :- 12
+print(math.gcd(12,18)) #output :- 6
+print(math.lcm(12,18)) #output :- 36
+print(math.pi) #output :- 3.141592653589793
+print(math.log(100)) #output :- 4.605170185988092
+print(math.log10(100)) #output :- 2.0
