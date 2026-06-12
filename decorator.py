ba6f77ab7282329003f66a076ef28a2d319e3025
ShauryaSingh1709 <shauryaa170904@gmail.com>
2026-06-12 10:21:45 +0530

    Shaurya

--- decorator.py
+++ decorator.py
@@ -5,3 +5,30 @@
 #         func(*args, **kwargs)
 #         post task
 #     return wrapper
+
+
+
+
+
+# def my_decorator (func):
+#     def wrapper():
+#         print("This is start of decorator")
+#         func()
+#         print("This is end of decorator")
+#     return wrapper
+# @my_decorator
+# def greet():
+#     print("Hello World")
+# greet()
+
+
+def shaurya (func):
+    def wrapper():
+        print("This is start of decorator")
+        func()
+        print("This is end of decorator")
+    return wrapper
+@shaurya
+def greet():
+    print("Hello World")
+greet()
