019d786aa853b5c252c1890123cb0490927d495c
ShauryaSingh1709 <shauryaa170904@gmail.com>
2026-06-17 10:38:50 +0530

    numpy

---
+++ numpy.py
@@ -0,0 +1,8 @@
+#Numpy is used to perform mathematical operations.
+#Numpy is a library which is used to work with arrays.
+#Numpy is a library which is used to work with matrices.
+
+import numpy as np #np is alias for numpy.
+np.__version__ #to get the version of numpy.
+b = np.array([1,2,3,4])
+print(b) #to print the array
