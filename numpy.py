01f025196fb6167d42c4870198c1a4203fa79940
ShauryaSingh1709 <shauryaa170904@gmail.com>
2026-06-17 10:41:30 +0530

    numpy

--- numpy.py
+++ numpy.py
@@ -6,3 +6,12 @@ import numpy as np #np is alias for numpy.
 np.__version__ #to get the version of numpy.
 b = np.array([1,2,3,4])
 print(b) #to print the array
+
+arr1 = np.array([1,2,3,4,5,6])
+arr2 = np.array([7,8,9,10,11,12]) 
+#this is how we create array in numpy.
+
+#In Numpy we have three types of Arrays:
+#1. 1D Array (Unidimensional Array)
+#2. 2D Array (Bidimensional Array)
+#3. 3D Array (Multidimensional Array)
