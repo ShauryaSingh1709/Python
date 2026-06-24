8cc7fa0735ed2bb38dc3767a12468d25e150adef
ShauryaSingh1709 <shauryaa170904@gmail.com>
2026-06-24 11:25:34 +0530

    ok

--- working_on_titanic_dataset.py
+++ working_on_titanic_dataset.py
@@ -8,3 +8,7 @@ print(shaurya[shaurya['embarked'] == 'S'])
 
 #Filter the dataset to find passengers who paid a fare between 20 and 40 units of currency.
 print(shaurya[(shaurya['fare'] >= 20) & (shaurya['fare'] <= 40)])
+
+
+#Filter the dataset to find passengers whose age is missing (Age = NaN)
+print(shaurya[shaurya['age'].isnull()])
