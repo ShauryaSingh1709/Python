398abac7657575fa3e279093c7e2bc1bb990a0e8
ShauryaSingh1709 <shauryaa170904@gmail.com>
2026-06-24 11:16:03 +0530

    ok

--- working_on_titanic_dataset.py
+++ working_on_titanic_dataset.py
@@ -1,3 +1,6 @@
 import pandas as pd
 shaurya = pd.read_csv('./Datasets/amazon_fires.csv', encoding = 'ISO-8859-1')
 print(shaurya)
+
+#Write a query to retrieve all passengers who embarked from southampton (Embarked = 'S')
+print(shaurya[shaurya['embarked'] == 'S'])
